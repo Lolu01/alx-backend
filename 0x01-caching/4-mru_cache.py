88e6ff80aedbb6f22555478c5aa608f91f59c1fe
Lolu01 <jaiyeolalulu@gmail.com>
2024-10-31 10:41:12 +0000

    0x01. Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,7 +2,7 @@
 """ module to implement MRUCache class """
 
 BaseCaching = __import__('base_caching').BaseCaching
-
+from collections import OrderedDict
 
 class MRUCache(BaseCaching):
     """ MRU cache class """
@@ -10,14 +10,16 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """ Constructor """
         super().__init__()
-        from collections import OrderedDict
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """ Add an item in the cache"""
         if key is None or item is None:
-            self.cache_data.move_to_end(key)
+            return
+
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             last_key = next(reversed(self.cache_data))
             del self.cache_data[last_key]
@@ -27,4 +29,5 @@ class MRUCache(BaseCaching):
         """ Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
+        self.cache_data.move_to_end(key)
         return self.cache_data[key]
